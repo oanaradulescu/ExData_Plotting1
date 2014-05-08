3ef5de4b52fd045b68cb1ed4bef0c77749cab19d
oanaradulescu <oana.a.radulescu@gmail.com>
2014-05-08 14:58:12 -0400

    plot1

--- plot1.R
+++ plot1.R
@@ -1,50 +1,34 @@
+# Assumes household_power_consumption.txt is in the working directory
+
 # Our overall goal here is simply to examine how household energy usage varies 
 # over a 2-day period in February, 2007. Your task is to reconstruct the following 
 # plots below, all of which were constructed using the base plotting system.
 
+install.packages("sqldf")
 library(sqldf)
 
 # build a tidy data set to support our goal
 
-powerfile <- "household_power_consumption.txt"
-powerfebsql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
-power <- read.csv2.sql(powerfile, powerfebsql)
-
-str(power)
-
-# rename existing Date column
-names(power)[1] <- "DateAsString"
-# add new Date column of type Sys.Date()
-power$Date <- as.Date(power$DateAsStr, "%d/%m/%Y")
-
-# Warning
-# This is a convenience function intended for use interactively. 
-# For programming it is better to use the standard subsetting arithmetic functions, 
-# and in particular the non-standard evaluation of argument transform 
-# can have unanticipated consequences.
-
-# power <- transform(power, Date = as.Date(power$DateAsStr, "%d/%m/%Y"))
-
-
-plot <- function() {
-#     par(fig=c(0, 1, .2, .8))
-    hist(power$Global_active_power, col="red", ylim=c(0,1200)
-         , main="Global Active Power"
-#        , cex.axis=0.7
-#        , cex.lab=0.8
-#        , cex.main=0.9
-         , xlab="Global Active Power (kilowatts)", ylab="Frequency"
-#        , mgp=c(2.25,1,0)
-
-    )
+tidy.power <- function() {
+    powerfile <- "household_power_consumption.txt"
+    powerfebsql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+    power <- read.csv2.sql(powerfile, powerfebsql)
+    power
 }
 
+power <- tidy.power()
 
+plot1 <- function() {
+    hist(power$Global_active_power, col="red"
+         , main="Global Active Power"
+         , xlab="Global Active Power (kilowatts)", ylab="Frequency"
+    )
+}
 
 png(file="plot1.png"
     , width=504
     , height=504
     , bg="transparent")
-plot()
+plot1()
 dev.off()
 
